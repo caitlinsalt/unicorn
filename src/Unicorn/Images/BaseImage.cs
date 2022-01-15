ea4820e9d3db2e931d154c9f540b6f760d186495
Caitlin Salt <forestpines@gmail.com>
2022-01-15 09:22:54 +0000

    Issue #20 WIP

---
+++ src/Unicorn/Images/BaseImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unicorn.CoreTypes;
+
+namespace Unicorn.Images
+{
+    public abstract class BaseImage : IImage
+    {
+        public abstract int DotWidth { get; }
+
+        public abstract int DotHeight { get; }
+
+        public double AspectRatio => (double)DotWidth / DotHeight;
+    }
+}
