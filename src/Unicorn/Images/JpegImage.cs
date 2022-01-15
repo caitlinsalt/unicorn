c7a8d9199a4ea15586f06e0b52dffe5d6ccc9e6c
Caitlin Salt <forestpines@gmail.com>
2022-01-15 09:22:54 +0000

    Issue #20 WIP

---
+++ src/Unicorn/Images/JpegImage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Unicorn.CoreTypes;
+
+namespace Unicorn.Images
+{
+    public class JpegImage : BaseImage
+    {
+        private Stream _dataStream;
+
+        public override int DotWidth => throw new NotImplementedException();
+
+        public override int DotHeight => throw new NotImplementedException();
+
+        public JpegImage(Stream stream)
+        {
+            _dataStream = stream;
+            stream.Seek(7, SeekOrigin.Begin);
+
+        }
+    }
+}
