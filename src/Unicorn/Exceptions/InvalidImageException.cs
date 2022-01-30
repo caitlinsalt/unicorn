c270cdcbcf7a72a27cd809e204de213354ae9618
Caitlin Salt <forestpines@gmail.com>
2022-01-30 09:33:33 +0000

    Issue #20 More progress on JPEG loading

---
+++ src/Unicorn/Exceptions/InvalidImageException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Unicorn.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when Unicorn cannot parse an image file sufficiently to embed it in a PDF.
+    /// </summary>
+    public class InvalidImageException : Exception
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public InvalidImageException() : base() { }
+
+        /// <summary>
+        /// Constructor with error message parameter.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        public InvalidImageException(string message) : base(message) { }
+
+        /// <summary>
+        /// Constructor with error message and triggering exception parameters.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        /// <param name="innerException">The exception whose throwing triggered this one.</param>
+        public InvalidImageException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
--- src/Unicorn/Images/BaseSourceImage.cs
+++ src/Unicorn/Images/BaseSourceImage.cs
@@ -1,16 +1,88 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Threading.Tasks;
 using Unicorn.CoreTypes;
 
 namespace Unicorn.Images
 {
-    public abstract class BaseSourceImage : ISourceImage
+    /// <summary>
+    /// Base class for classes which load and partially parse raster image files.
+    /// </summary>
+    public abstract class BaseSourceImage : ISourceImage, IDisposable
     {
-        public abstract int DotWidth { get; }
+        private protected readonly MemoryStream _dataStream = new MemoryStream();
+        private bool disposedValue;
 
-        public abstract int DotHeight { get; }
+        /// <summary>
+        /// The image width in pixels.
+        /// </summary>
+        public virtual int DotWidth { get; protected set; }
 
+        /// <summary>
+        /// The image height in pixels.
+        /// </summary>
+        public virtual int DotHeight { get; protected set; }
+
+        /// <summary>
+        /// The image aspect ratio, as a fraction.
+        /// </summary>
         public double AspectRatio => (double)DotWidth / DotHeight;
+
+        /// <summary>
+        /// Load the image from a stream.
+        /// </summary>
+        /// <param name="stream">The source of the image data.</param>
+        /// <returns>A <see cref="Task" /> which will be completed when the image is loaded.</returns>
+        /// <exception cref="ArgumentNullException">The <c>stream</c> parameter is null.</exception>
+        /// <exception cref="ObjectDisposedException">The <c>stream</c> parameter is disposed, or the <see cref="BaseSourceImage" /> object is disposed.</exception>
+        /// <exception cref="NotSupportedException">The <c>stream</c> parameter does not support reading.</exception>
+        /// <remarks>
+        /// <para>
+        /// Depending on the type of <see cref="Stream" /> passed to this method, this method may throw additional exception types not listed here.
+        /// </para>
+        /// <para>
+        /// This method loads the entire data stream into memory.  It is the caller's responsibility to confirm that the data stream is not excessively large.
+        /// </para>
+        /// </remarks>
+        public virtual async Task LoadFromAsync(Stream stream)
+        {
+            if (stream is null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+            await stream.CopyToAsync(_dataStream).ConfigureAwait(false);
+            _dataStream.Seek(0, SeekOrigin.Begin);
+        }
+
+        #region Dispose pattern implementation
+
+        /// <summary>
+        /// Releases all resources used by the <see cref="BaseSourceImage" /> object.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release all resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _dataStream.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        /// <summary>
+        /// Releases all resources used by the <see cref="BaseSourceImage" /> object.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
     }
 }
