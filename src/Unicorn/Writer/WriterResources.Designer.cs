e3c0f480edb6fae8974b3bf5e943a8d232d18eac
Caitlin Salt <forestpines@gmail.com>
2022-02-04 08:32:52 +0000

    Issue #96 Rename resources files

--- src/Unicorn/Writer/WriterResources.Designer.cs
+++ src/Unicorn/Writer/WriterResources.Designer.cs
@@ -22,14 +22,14 @@ namespace Unicorn.Writer {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    internal class WriterResources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources() {
+        internal WriterResources() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Unicorn.Writer {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Unicorn.Writer.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Unicorn.Writer.WriterResources", typeof(WriterResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
