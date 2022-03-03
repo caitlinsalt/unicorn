337c761280e97d8b20a7b9a69381d2ab2ad5a00a
Caitlin Salt <forestpines@gmail.com>
2022-03-03 07:08:29 +0000

    Issue #121 Initial work, not reading internal pointers correctly

---
+++ src/Unicorn/Images/Jpeg/ExifStorageType.cs
@@ -0,0 +1,15 @@
+﻿namespace Unicorn.Images.Jpeg
+{
+    internal enum ExifStorageType
+    {
+        Byte         = 1,     // Unsigned byte.
+        Ascii        = 2,     // String.
+        Short        = 3,     // Unsigned short.
+        Long         = 4,     // Unsigned int.
+        Rational     = 5,     // Fraction of two unsigned ints.
+        Undefined    = 7,     // Unsigned byte
+        Slong        = 9,     // Signed int
+        Srational    = 10,    // Fraction of two signed ints.
+        ReadFromFile = 255,   // This tag can vary in type depending on the writer.
+    }
+}
