3d949706bc8cf52d4273d05ffa64f8fb296c433d
Caitlin Salt <forestpines@gmail.com>
2022-03-12 08:48:47 +0000

    Issue #142 Refactor SOF segment loading code

---
+++ src/Unicorn/Images/Jpeg/StartOfFrameSegment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Unicorn.Base.Helpers.Extensions;
+using Unicorn.Exceptions;
+
+namespace Unicorn.Images.Jpeg
+{
+    internal class StartOfFrameSegment : JpegDataSegment
+    {
+        internal int DotWidth { get; private set; }
+
+        internal int DotHeight { get; private set; }
+
+        internal StartOfFrameSegment(long startOffset, int length) : base(startOffset, length, JpegDataSegmentType.StartOfFrame) { }
+
+        internal override async Task PopulateSegmentAsync(Stream dataStream)
+        {
+            const int bufferOffset = 5;
+            const int bufferLength = 4;
+            const int widthOffsetInBuffer = 2;
+            byte[] buffer = new byte[bufferLength];
+            dataStream.Seek(StartOffset + bufferOffset, SeekOrigin.Begin);
+            int bytesRead = await dataStream.ReadAsync(buffer, 0, bufferLength).ConfigureAwait(false);
+            if (bytesRead < bufferLength)
+            {
+                throw new InvalidImageException(ImageLoadResources.JpegSourceImage_DimensionsNotFound);
+            }
+
+            DotHeight = buffer.ReadBigEndianUShort();
+            DotWidth = buffer.ReadBigEndianUShort(widthOffsetInBuffer);
+        }
+    }
+}
